f7ae219b71cdafc83a276d42b14c07a81c9bdd67
shermuhammad temirov <temirov_s@qwasar.io>
2022-12-13 12:34:24 +0000

    I fixed

--- my_functions.c
+++ my_functions.c
@@ -15,24 +15,6 @@ int my_strlen(char* characters)
     return answer;
 }
 
-int my_num(char* characters)
-{
-    int number = 0;
-    int cycle = 0;
-    for(int z = 0; characters[z] != '\0'; z++)
-    {
-        if(characters[z] >= '0' && characters[z] <= '9')
-        {
-            number++;
-        }
-        cycle++;
-    }
-    if(cycle == number)
-    {
-        return 1;
-    }
-    return 0;
-}
 
 char* my_random()
 {
@@ -42,8 +24,8 @@ char* my_random()
     for(int n = 0; n != 4; n++)
     {
         copy = rand() % 9;
-		copy += 48;
-		
+        copy += 48;
+        
         answer[n] = copy; 
     }
    
@@ -51,199 +33,129 @@ char* my_random()
 }
 
 //FOR THE MAIN FUNCTION
-
-//function for secret code
 char* scode(int size, char **arr)
 {
-    //This function accepts 4 numbers from the terminal using the -c command. 
-    //If the numbers are entered incorrectly or not, it returns 4 numbers from 0 to 8;
-    
     char* answer = (char*) malloc(4 * sizeof(char));
 
-      
-    //We look for the -c command;
     for(int n = 1; n != size; n++)
     {
-        if(my_strlen(arr[n]) == 2)
+        if(my_strlen(arr[n]) == 2 && arr[n][0] == '-' && arr[n][1] == 'c')
         {
-            if(arr[n][0] == '-' && arr[n][1] == 'c')
-            {
-                //If information is entered after -c command and consists of 4 characters;
-                if(size >= n+1 && my_strlen(arr[n+1] ) == 4)
-                {
-                    int count = 0;
-                    //If characters 1 -> 8 are entered in reference;
-                    for(int i = 0; i != 4; i++)
-                    {                       
-                        if(arr[n+1][i] >= '0' && arr[n+1][i] <= '8')
-                        {                                                    
-                            count++;
-                        }
-                        //Otherwise, return 4 random numbers from 0 to -> 8;
-                        else 
-                        {   
-                            
-                            answer = my_random();
-                            return answer;
-                        }
-                    }
-                    if(count == 4)
-                    {
-                        for(int i = 0; i != 4; i++)
-                        {
-                            answer[i] = arr[n+1][i];
-                        }
-                        return answer;
-                    }
-                    //Otherwise, return 4 random numbers from 0 to -> 8;
-                    // printf("respons: %s\n", respons);
-                    
-                    // printf("\n");
-                    // printf("function answer: %s\n", *answer);
-                    
+            int count = 0;
+            for(int i = 0; i != 4; i++)
+            {                       
+                if(arr[n+1][i] >= '0' && arr[n+1][i] <= '8')
+                {                                                    
+                    count++;
+                }
+                else 
+                {                               
+                    answer = my_random();
                     return answer;
                 }
             }
+
+            if(count == 4)
+            {
+                answer[0] = arr[n+1][0];
+                answer[1] = arr[n+1][1];
+                answer[2] = arr[n+1][2];
+                answer[3] = arr[n+1][3];
+                return answer;
+            }
+            answer = my_random();
+            return answer;
         }
     }
-
-
-    //Otherwise, return 4 random numbers from 0 to -> 8;
     answer = my_random();
     return answer;
 }
 
-//function that returns rounds;
+
 int sycle(int size, char** arr)
 {
-    //We look for the -t command in the given array;
     for(int n = 1; n != size; n++)
     {
         if(my_strlen(arr[n]) == 2)
         {
             if(arr[n][0] == '-' && arr[n][1] == 't')
             {
-                // If a number is entered after the -t command, return that number;
-                if(size >= n+1)
-                {
-                    if(my_num(arr[n+1]) > 0)
-                    {
-                        return atoi(arr[n+1]);
-                    }
-                }
+                return atoi(arr[n+1]);
             }
         }
     }
-
-    // otherwise return 10;
     return 10;
 } 
 
-//function for well place;
+
 int wellp(char* answer, char* correct_a)
 {
     int count = 0;
-	for(int n = 0; n != 4; n++)
-	{
-        // printf("answer: %c correct : %c\n", answer[n], correct_a[n]);
-		if(answer[n] == correct_a[n])
-		{
-			count++;
-		}
-	}
-	
-	return count;
+    for(int n = 0; n != 4; n++)
+    {
+        if(answer[n] == correct_a[n])
+        {
+            count++;
+        }
+    }
+
+    return count;
 }
 
-//function for miss place;
+
 int misp(char* answer, char* correct_a)
 {
     int count = 0;
-	for(int n = 0; n != 4; n++)
-	{
-		if(answer[n] != correct_a[n])
-		{
-			for(int i = 0; i != 4; i++)
-			{
-				if(answer[n] == correct_a[i])
-				{
-					count++;
-				}
-			}
-		}
-	}
-	
-	
-	
-	return count;
-}
-
-//function for intput;
-char* input()
-{
-    char* answer = (char*) malloc(4 * sizeof(char));
-    int sign = 1;
-    char ram;
-    int index = 0;
-    int count = 0;
-
-    //until the correct answer is entered;
-    while (sign > 0)
+    for(int n = 0; n != 4; n++)
     {
-        //If incorrect information is entered;
-        if(index > 0)
+        if(answer[n] != correct_a[n])
         {
-            write(0, "Wrong input!\n", 13);
-        }
-        ram = ' ';
-        write(0, ">", 1);
-        int n;
-
-        //Read all the information entered in the terminal and upload 4 characters to the answer
-        for(n = 0; ram != '\n'; n++)
-        {
-            read(0, &ram, 1);
-            // printf("%c ", ram);
-            // printf("size: %d ", n);
-
-            if(n < 4)
-            {
-                answer[n] = ram;
-            }        
-        }
-        //If the entered information consists of 4 characters:
-        if(n == 5)
-        {
-            count = 0;
             for(int i = 0; i != 4; i++)
             {
-                //if each character of answer consists of a character from 0 to -> 8:
-                if(answer[i] >= '0' && answer[i] <= '8')
+                if(answer[n] == correct_a[i])
                 {
                     count++;
                 }
             }
-            //return the answer;
-            if(count == 4)
-            {
-                break;
-            }
-            //otherwise prompt for re-entry;
-            else 
-            {
-                sign++;
-            }
         }
-        //therwise, ask to enter the information again;
-        else 
-        {
-            sign++;
-        }
-
-        sign--;
-        index++;
     }
+
+    return count;
+}
+
+
+char* input()
+{
+    char* depo = (char*) malloc(4 * sizeof(char));
+    char ram;
+
+    start:
+        free(depo);
+        ram = ' ';
+        write(0, ">", 1);
+        int n;
     
-    
-    return answer;
+        for(n = 0; ram != '\n'; n++)
+        {
+            read(0, &ram, 1);
+            
+
+            if(n < 4)
+            {
+            }
+                if(ram >= '0' && ram <= '8')
+                {
+                    // write(0,"|", 1);
+                    depo[n] = ram;
+                }   
+                else
+                {
+                    write(0, "Wrong input!\n", 13);
+                    goto start;
+                }    
+                        
+        }
+        return depo;
+    // write(0, "Wrong input!\n", 13);
+    goto start;
 }
--- my_mastermind.c
+++ my_mastermind.c
@@ -4,40 +4,25 @@
 int main(int size, char* array[])
 {
     char* secret_code = scode(size, array);
-
-    printf("secret code : %s\n", secret_code);
-    
-   
     int my_cycle = sycle(size, array);
-    printf("my_cycle: %d\n", my_cycle);
 
     printf("Will you find the secret code?\n");
     printf("Please enter a valid guess\n");
-    
-    int round = 0;
+
     char* input_number;
     int well;
     int miss;
 
-
-    for(int n = 0; n != my_cycle; n++)
+    for(int n = 0; n < my_cycle; n++)
     {
-        
-        printf("---\n");
-        printf("Round %d\n", round);
-
+        printf("---\nRound %d\n", n);
         input_number = input();
-
 
         // printf("secret code : %s\n", secret_code);
         // printf("input number : %s\n", input_number);
         
-    
-
         well = wellp(input_number, secret_code);
         miss = misp(input_number, secret_code);
-        // printf("miss: %d\n", miss);
-        // printf("well: %d\n", well);
 
         if(well == 4)
         {        
@@ -47,8 +32,6 @@ int main(int size, char* array[])
 
         printf("Well placed pieces: %d\n", well);
         printf("Misplaced pieces: %d\n", miss);
-        
-        round++;
     }
 
     return 0;
